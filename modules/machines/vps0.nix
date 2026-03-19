addf7d2233444b5bb20b73f88476b60c83f80635
pmeinhold <paulmatti@protonmail.com>
2026-03-19 10:25:20 +0100

    add hetzner vps

---
+++ modules/machines/vps0.nix
@@ -0,0 +1,60 @@
+{ lib, config, inputs, ... }:
+# let
+#   domain = "getintogig.duckdns.org";
+#   username = "paulm";
+# in
+{
+  flake.nixosConfigurations.srvr = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      config.flake.modules.nixos.feature-base
+
+      ({ config, lib, pkgs, ... }: {
+        networking.hostName = "vps0";
+
+        boot.loader.grub.enable = true;
+        boot.loader.grub.device = "/dev/sda";
+        boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-label/nixos";
+          fsType = "ext4";
+        };
+        fileSystems."/boot" = {
+          device = "/dev/disk/by-label/boot";
+          fsType = "ext4";
+        };
+
+        users.users.root.hashedPassword = "!"; # Disable root login
+
+        security.sudo.wheelNeedsPassword = false;
+
+        networking.firewall = {
+          enable = true;
+          allowedTCPPorts = [
+            69
+            80
+            443
+          ];
+          allowedUDPPorts = [
+          ];
+        };
+
+        services = {
+          openssh = {
+            enable = true;
+            ports = [ 69 ];
+            settings = {
+              PermitRootLogin = "no";
+              PasswordAuthentication = false;
+              KbdInteractiveAuthentication = false;
+            };
+          };
+        };
+
+        system.stateVersion = "25.11";
+      })
+
+    ];
+  };
+}
