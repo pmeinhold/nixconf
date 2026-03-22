c5ff36feebb97b014f81acf450ba6523f6720b98
pmeinhold <paulmatti@protonmail.com>
2026-03-22 18:05:01 +0100

    added tini

---
+++ modules/machines/tini/default.nix
@@ -0,0 +1,43 @@
+{ lib, config, inputs, ... }:
+{
+  flake.nixosConfigurations.tini = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      ./_hardware.nix
+      config.flake.modules.nixos.feature-base
+      config.flake.modules.nixos.feature-desktop
+
+      ({ ... }: {
+        networking.hostName = "tini";
+
+        boot.loader.grub = {
+          enable = true;
+          device = "nodev";
+        };
+
+        services.desktopManager.gnome.enable = true;
+        services.displayManager.autoLogin = {
+          enable = true;
+          user = "paulm";
+        };
+
+        services.openssh = {
+          enable = true;
+          settings = {
+            PermitRootLogin = "no";
+            PasswordAuthentication = true;
+          };
+        };
+
+        programs.steam = {
+          enable = true;
+          remotePlay.openFirewall = false;
+          dedicatedServer.openFirewall = false;
+        };
+
+        system.stateVersion = "25.11";
+      })
+
+    ];
+  };
+}
--- modules/users.nix
+++ modules/users.nix
@@ -64,6 +64,22 @@
     ];
   };
 
+  flake.homeConfigurations."paulm@tini" = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    modules = [
+      config.flake.modules.homeManager.feature-base
+      config.flake.modules.homeManager.feature-shell
+      config.flake.modules.homeManager.feature-desktop
+      config.flake.modules.homeManager.feature-emulation
+
+      ({ pkgs, ... }: {
+        programs.retroarch.settings.video_driver = "glcore"; # vulkan, glcore, gl, gl1, sdl
+        home.stateVersion = "25.11";
+      })
+
+    ];
+  };
+
   flake.homeConfigurations."paulm@deck" = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     modules = [
