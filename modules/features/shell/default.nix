07eab4512abfa4ed6e47fcddb997c51d5583c98e
pmeinhold <paulmatti@protonmail.com>
2026-05-29 14:18:19 +0200

    update

--- modules/features/shell/default.nix
+++ modules/features/shell/default.nix
@@ -31,7 +31,32 @@
         set -U fish_greeting
       '';
     };
-    programs.opencode.enable = true;
+    programs.opencode = {
+      enable = true;
+      settings = {
+        provider = {
+          zib = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "ZIB";
+            options = {
+              baseURL = "https://ollama.zib.de/api";
+              # apiKey = "{env:ZIB_API_KEY}";
+            };
+            models = {
+              "llama3.2:latest" = {
+                name = "llama3.2:latest";
+                # options = {
+                #   structuredOutputs = false;
+                # };
+              };
+              "deepseek-coder-v2:latest".name = "deepseek-coder-v2:latest";
+              "deepseek-r1:14b".name = "deepseek-r1:14b";
+              "llama3:70b".name = "llama3:70b";
+            };
+          };
+        };
+      };
+    };
     programs.yazi.enable = true;
     programs.zoxide = {
       enable = true;
