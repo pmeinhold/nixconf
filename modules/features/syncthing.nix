a80630bb8dd47527fa5890072c48b44aa91200df
pmeinhold <paulmatti@protonmail.com>
2026-08-17 12:41:54 +0200

    update

--- modules/features/syncthing.nix
+++ modules/features/syncthing.nix
@@ -4,27 +4,39 @@ let
   defaultUser = "paulm";
 in
 {
-  flake.modules.nixos.feature-syncthing = { config, ... }:
+  flake.modules.nixos.feature-syncthing = { config, pkgs, ... }:
   {
+
+    age.identityPaths = [ "/home/${defaultUser}/.ssh/id_ed25519" ];
+    age.secrets.syncthing = {
+      file = ../../secrets/syncthing_gui_pw.age;
+      owner = "${defaultUser}";
+    };
+
     services.syncthing = {
-      # enable = true;
+      enable = true;
       openDefaultPorts = true;
-      user = "${defaultUser}";
+      user = "${defaultUser}"; # breaks if group = "users"; is not set
+      group = "users";
+      dataDir = "/home/${defaultUser}/";
       guiAddress = if config.networking.hostName == "srvr" then "0.0.0.0:8384" else "127.0.0.1:8384";
+      guiPasswordFile = config.age.secrets.syncthing.path;
       settings = {
         gui.user = "${defaultUser}";
-        # devices = {
-        #   "device1" = { id = "DEVICE-ID-GOES-HERE"; };
-        #   "device2" = { id = "DEVICE-ID-GOES-HERE"; };
-        # };
+        devices = {
+          "zenfone" = { id = "YGHKFE7-CKPD3XY-EJQJWSJ-7VA6N35-2LENZ7H-PE2CEBR-WFFXDQN-KIBVPQ2"; };
+        };
         folders."Sync" = {
           path = "/home/${defaultUser}/Sync";
-          # devices = [ "device1" "device2" ];
+          devices = [
+            "zenfone"
+          ];
         };
       };
-      # dataDir = "/home/${defaultUser}/";
-      # settings.folders."Sync".path = "/home/${defaultUser}/Sync";
     };
+
+    # Encrypt/decrypt
+    environment.systemPackages = with pkgs; [ gocryptfs ];
   };
 
   flake.modules.homeManager.feature-syncthing = { ... }:
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,33 @@
 # https://github.com/ryantm/agenix?tab=readme-ov-file#tutorial
 let
-  # user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4vpPLYf+6rzvDomLju8J+X7oOKxGNhN2C41sUx0b90";
+  # User pub keys
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4vpPLYf+6rzvDomLju8J+X7oOKxGNhN2C41sUx0b90";
+  users = [ default ];
+
+  # System pub keys
   srvr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6MUaxloE+8Vxj9bZ5MvFJXtuDlqST5HqwD5q+7VfMc root@nixos";
+  systems = [ srvr ];
 in
 {
   # "secret1.age".publicKeys = [ user1 ];
   "duckdns_token.age".publicKeys = [ srvr ];
+  "syncthing_gui_pw.age".publicKeys = [ default ];
 }
+
+# Usage:
+#   cd secrets/
+#   agenix -e <secret>.age
+#
+# Then reference it like this:
+#   age.secrets.nextcloud = {
+#     file = ./secrets/secret1.age;
+#     owner = "nextcloud";
+#     group = "nextcloud";
+#   };
+#   services.nextcloud = {
+#     enable = true;
+#     package = pkgs.nextcloud28;
+#     hostName = "localhost";
+#     config.adminpassFile = config.age.secrets.nextcloud.path;
+#   };
+# https://wiki.nixos.org/wiki/Agenix
